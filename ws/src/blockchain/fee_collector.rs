13bff8a6930abbfdf5731d1cd97f6be5736ed363
ppoliani <ppoliani@gmail.com>
2024-08-14 16:30:32 +0300

    feat: add solana creates

---
+++ ws/src/blockchain/fee_collector.rs
@@ -0,0 +1,14 @@
+// use std::pin::Pin;
+// use async_stream::try_stream;
+// use futures::Stream;
+// use eyre::Result;
+
+// pub struct FeeCollector {
+//   stream: Pin<Box<dyn Stream<Item = Result<()>> + Send>>,
+// }
+
+// impl FeeCollector {
+//   pub fn collect() -> Self {
+
+//   }
+// }
