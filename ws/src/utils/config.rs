6c6c4cdcacddfb428619a7f714504772bafa5253
ppoliani <ppoliani@gmail.com>
2024-08-14 16:31:22 +0300

    feat: add rpc_client to the store

--- ws/src/utils/config.rs
+++ ws/src/utils/config.rs
@@ -4,4 +4,6 @@ use envconfig::Envconfig;
 pub struct Config {
   #[envconfig(from = "PORT")]
   pub port: u64,
+  #[envconfig(from = "SOLANA_RPC")]
+  pub solana_rpc: String,
 }
--- ws/src/utils/store.rs
+++ ws/src/utils/store.rs
@@ -1,14 +1,20 @@
 use envconfig::Envconfig;
+use solana_client::nonblocking::rpc_client::RpcClient;
 use super::config::Config;
 
 pub struct Store {
   pub config: Config,
+  pub rpc_client: RpcClient,
 }
 
 impl Store {
   pub async fn new() -> Self {
     let config = Config::init_from_env().unwrap();
+    let rpc_client = RpcClient::new(config.solana_rpc.clone());
 
-    Self {config}
+    Self {
+      config,
+      rpc_client,
+    }
   }
 }
