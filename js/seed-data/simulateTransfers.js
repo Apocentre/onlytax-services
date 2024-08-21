604bf6ff281c93c560db77d8bc033d8a762900eb
ppoliani <ppoliani@gmail.com>
2024-08-21 16:30:27 +0300

    feat: add logic to deserialize sign and send theWithdrawWithheldTokensFromAccounts tx

--- js/seed-data/simulateTransfers.js
+++ js/seed-data/simulateTransfers.js
@@ -1,4 +1,4 @@
-import {Keypair, PublicKey, Connection} from "@solana/web3.js"
+import {Keypair, PublicKey} from "@solana/web3.js"
 import config from "./.config.json" assert { type: "json" };
 import {createTestAccounts, tranfer} from "./common.js"
 
--- js/seed-data/wsClient.js
+++ js/seed-data/wsClient.js
@@ -1,5 +1,10 @@
 import {io} from "socket.io-client";
-import {Transaction} from "@solana/web3.js";
+import {VersionedTransaction, Connection} from "@solana/web3.js";
+import {Keypair} from "@solana/web3.js"
+import config from "./.config.json" assert { type: "json" };
+
+const withdrawWithheldAuthority = Keypair.fromSecretKey(Buffer.from(config.withdrawWithheldAuthorityKey));
+const connection = new Connection(config.rpc);
 
 const main = async () => {
   const socket = io("ws://localhost:8090", {
@@ -10,11 +15,16 @@ const main = async () => {
     socket.emit("collect", "9NxTF8W3gB1y49LBn1GTp5QqPmkdp4P8HJDiqJgJQSUB", "681gMAUpbqTms3RW773mHXjx8hvw4SyosjC2ZcizG6bd");
     socket.emit("new-tokens");
 
-    socket.on("681gMAUpbqTms3RW773mHXjx8hvw4SyosjC2ZcizG6bd", (msg) => {
+    socket.on("681gMAUpbqTms3RW773mHXjx8hvw4SyosjC2ZcizG6bd", async (msg) => {
       console.log("New encoded transaction", msg);
 
-      const tx = Transaction.from(msg.data);
-      console.log("Decoded transaction", tx);
+      const tx = VersionedTransaction.deserialize(msg.data);
+      let {blockhash} = await connection.getLatestBlockhash("confirmed");
+      tx.message.recentBlockhash = blockhash;
+      tx.sign([withdrawWithheldAuthority]);
+
+      const txid = await connection.sendTransaction(tx, {maxRetries: 5, skipPreflight: false});
+      console.log("Transaction sent", txid);
     });
   });
 
