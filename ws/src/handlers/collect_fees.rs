fa526950323ed7e8b27f014859039918b2634c6e
ppoliani <ppoliani@gmail.com>
2024-08-24 13:59:22 +0300

    chore: rename to response

--- ws/src/handlers/collect_fees.rs
+++ ws/src/handlers/collect_fees.rs
@@ -8,8 +8,8 @@ use solana_sdk::pubkey::Pubkey;
 use crate::utils::store::Store;
 
 #[derive(Serialize)]
-struct SerializedTx {
-  data: Option<Vec<u8>>,
+struct Response {
+  tx: Option<Vec<u8>>,
   count: usize,
 }
 
@@ -25,14 +25,14 @@ pub async fn exec(
   let mut stream = store.fee_collector.collect(&mint, &withdraw_withheld_authority_key);
 
   while let Some(Ok(item)) = stream.next().await {
-    if let Err(err) = socket.emit(room.clone(), SerializedTx {data: Some(item.0), count: item.1}) {
+    if let Err(err) = socket.emit(room.clone(), Response {tx: Some(item.0), count: item.1}) {
       error!("failed to push new_batch for authority {} {}", withdraw_withheld_authority, err);
     }
   }
 
   // This msg will indicate the end of the stream. Usefull for WS clients to know when
   // the transactions are all consumed
-  if let Err(err) = socket.emit(room.clone(), SerializedTx {data: None, count: 0}) {
+  if let Err(err) = socket.emit(room.clone(), Response {tx: None, count: 0}) {
     error!("failed to push batch-complete for authority {} {}", withdraw_withheld_authority, err);
   }
 
