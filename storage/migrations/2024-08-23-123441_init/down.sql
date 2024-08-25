b72724ad02a951b0e8dac9cfe1ec29e02282442c
ppoliani <ppoliani@gmail.com>
2024-08-25 14:01:26 +0300

    feat: add tokens and transactions table definitions

--- storage/migrations/2024-08-23-123441_init/down.sql
+++ storage/migrations/2024-08-23-123441_init/down.sql
@@ -1,2 +1,4 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE accounts CASCADE;
+DROP TABLE tokens CASCADE;
+DROP TABLE transactions CASCADE;
--- storage/migrations/2024-08-23-123441_init/up.sql
+++ storage/migrations/2024-08-23-123441_init/up.sql
@@ -5,3 +5,15 @@ CREATE TABLE accounts (
   soft_delete BOOLEAN NOT NULL DEFAULT FALSE,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+
+CREATE TABLE tokens (
+  address VARCHAR(44) PRIMARY KEY,
+);
+
+CREATE TABLE transactions (
+  withdraw_withheld_authority VARCHAR(44) NOT NULL REFERENCES accounts(address),
+  token VARCHAR(44) NOT NULL REFERENCES tokens(address),
+  batch_size INT NOT NULL DEFAULT FALSE,
+  tx_signature VARCHAR,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
